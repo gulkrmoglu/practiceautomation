6751e7d6258a95f83e74fec580193d7e00a35857
GÜL KARAMAHMUTOGLU <krmoglugul@gmail.com>
2024-06-17 21:14:41 +0300

    gk

--- practice_automation/pages/shoppage.py
+++ practice_automation/pages/shoppage.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from practice_automation.pages.constants.globalConstants import *
--- practice_automation/tests/test_register.py
+++ practice_automation/tests/test_register.py
@@ -1,19 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 from practice_automation.pages.register import Register
 from practice_automation.pages.constants.globalConstants import *
 
-options = Options()
-options.add_argument("start-maximized")
 
 class TestRegister:
 
     @pytest.fixture(autouse=True)
     def setup_and_teardown(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get(BASE_URL)
         yield
